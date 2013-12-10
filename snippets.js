aa2c5bf4cadf289aa5a4098efb9be1261163dadf
Tomusdrw <tomusdrw@gmail.com>
2013-12-10 23:24:02 +0100

    Added few snippets.

---
+++ snippets.js
@@ -0,0 +1,12 @@
+
+// Initial content of main.js
+var todos = [
+    "Checkout project from github",
+    "Invoke 'play run' in your console",
+    "Open your browser and head over to http://localhost:9000",
+    "Learn JS essentials"
+];
+
+console.log(todos);
+
+// Iterating over 
